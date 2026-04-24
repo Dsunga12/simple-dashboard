4b96bf48e7bab8a761cc2fcafee59f6e6b102da8
Dsunga12 <danielsunga03@gmail.com>
2026-04-24 18:18:26 +0800

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>🔥 Dashboard is Working!</h1>
+      <p>Your Vercel deployment is successful.</p>
+    </div>
+  );
+}
