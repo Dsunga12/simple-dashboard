ad1cd52c08867b976940ef082ac452aae3e00b0b
Dsunga12 <danielsunga03@gmail.com>
2026-05-18 20:50:03 +0800

    Delete src directory

--- src/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", color: "white" }}>
-      <h1>🔥 Dashboard is Working!</h1>
-      <p>Your Vercel deployment is successful.</p>
-    </div>
-  );
-}
